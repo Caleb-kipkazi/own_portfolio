58ce333f82ece53d0ef51b77cebd7c5b664d30fe
Caleb-kipkazi <calebkipkazi@gmail.com>
2026-02-10 15:22:25 +0300

    updated router.py to include HEAD

--- server/server.py
+++ server/server.py
@@ -13,6 +13,6 @@ class RequestHandler(BaseHTTPRequestHandler):
         route_request(self, "HEAD")
 
 if __name__ == "__main__":
-    server = HTTPServer(("localhost", 8000), RequestHandler)
+    server = HTTPServer(("0.0.0.0", 8000), RequestHandler)
     print("Server running at http://localhost:8000")
     server.serve_forever()
