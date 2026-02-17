42a283045474d1ae3847c151cf50037ab496db54
Caleb-kipkazi <calebkipkazi@gmail.com>
2026-02-17 06:32:49 +0300

    updated router.py to include HEAD

--- server/server.py
+++ server/server.py
@@ -1,23 +1,28 @@
+import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from server.router import route_request
-import os
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        route_request(self, method="GET")
+        route_request(self, "GET")
 
     def do_POST(self):
-        route_request(self, method="POST")
+        route_request(self, "POST")
 
     def do_HEAD(self):
         route_request(self, "HEAD")
 
+    # Optional: remove default logging noise
+    def log_message(self, format, *args):
+        return
+
+
 if __name__ == "__main__":
-    # 🔴 THIS IS THE MOST IMPORTANT PART
     PORT = int(os.environ.get("PORT", 8000))
 
     server = HTTPServer(("0.0.0.0", PORT), RequestHandler)
 
-    print(f"Server running on port {PORT}")  # NOT localhost
+    print(f"Server running on port {PORT}")
     server.serve_forever()
